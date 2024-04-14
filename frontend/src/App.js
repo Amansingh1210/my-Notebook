32f644e733d8b66192c1b6b9e77a347de04deaef
Aman Singh <singhaman38809@gmail.com>
2024-04-14 12:17:34 +0530

    Add the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-
+import Navbar from "./components/navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+          <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+
+        </Routes>
+
+      </BrowserRouter >
+
+
+    </>
   );
 }
 
---
+++ frontend/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <h1>This is about</h1>
+  )
+}
+
+export default About
